6ddb8e1ff36f37ff5a5fa451811c189c1f663886
Jon <stricoff92@gmail.com>
2022-01-30 11:34:05 -0500

    added models

--- eazyinvoice/app/models.py
+++ eazyinvoice/app/models.py
@@ -1,3 +1,127 @@
-from django.db import models
 
-# Create your models here.
+import uuid
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserProfile(BaseModel):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    legal_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address1 = models.TextField()
+    address2 = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    city = models.CharField(
+        max_length=255,
+    )
+    state = models.CharField(
+        max_length=2,
+    )
+    zipcode = models.CharField(
+        max_length=24,
+    )
+
+
+class Organization(BaseModel):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    short_name = models.CharField(max_length=24)
+    legal_name = models.CharField(max_length=255)
+    attn = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default=None,
+    )
+
+    address1 = models.TextField()
+    address2 = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    city = models.CharField(
+        max_length=255,
+    )
+    state = models.CharField(
+        max_length=2,
+    )
+    zipcode = models.CharField(
+        max_length=24,
+    )
+
+
+class Invoice(BaseModel):
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+    )
+    issued_date = models.DateField()
+    due_date = models.DateField()
+    invoice_number = models.CharField(
+        max_length=255,
+    )
+
+    class Meta:
+        unique_together = (
+            'organization',
+            'invoice_number',
+        )
+
+
+class HourlyRate(BaseModel):
+    rate = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+    )
+
+
+class HoursEntry(BaseModel):
+    date = models.DateField()
+    rate = models.ForeignKey(
+        HourlyRate,
+        on_delete=models.CASCADE,
+    )
+    invoice = models.ForeignKey(
+        Invoice,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    quantity = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+    description = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default=None,
+    )
