84428e59d90ad49764a5c6ac4fdf11257d671aee
Jon <stricoff92@gmail.com>
2022-04-10 13:33:22 -0400

    wip

--- eazyinvoice/eazyinvoice/urls.py
+++ eazyinvoice/eazyinvoice/urls.py
@@ -2,8 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseNotFound
 
 urlpatterns = [
     path('', include("api.urls")),
+    path('admin/login/', lambda requst: HttpResponseNotFound()),
+    path('admin/logout/', lambda requst: HttpResponseNotFound()),
     path('admin/', admin.site.urls),
 ]
