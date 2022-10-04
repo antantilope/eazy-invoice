388e3122e215e7920dcccf70b387fc52bf7b219e
Jon Stricoff <stricoff92@gmail.com>
2022-10-04 17:06:42 -0400

    wip

--- eazyinvoice/icit/models.py
+++ eazyinvoice/icit/models.py
@@ -6,18 +6,14 @@ from django.contrib.auth import get_user_model
 
 
 class IcitState(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-
     user = models.OneToOneField(
         get_user_model(),
         on_delete=models.CASCADE,
     )
-
     should_be_logged_in = models.BooleanField()
 
 
 class IcitMessage(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         get_user_model(),
